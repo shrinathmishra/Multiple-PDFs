8fcc6e261647e0bc9b055b2c557c1655a6c56912
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-11-05 16:07:40 +0530

    Uploading Application Class

---
+++ src/test/java/com/example/MultiplePDFs/MultiplePdFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MultiplePDFs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplePdFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
