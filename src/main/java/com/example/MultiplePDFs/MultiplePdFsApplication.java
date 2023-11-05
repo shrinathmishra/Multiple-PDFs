c95a923a8febb2e86f3b00609a815cf0f1c5cba2
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-11-05 16:07:22 +0530

    Uploading Application class

---
+++ src/main/java/com/example/MultiplePDFs/MultiplePdFsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MultiplePDFs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplePdFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplePdFsApplication.class, args);
+	}
+
+}
